73d01f9f759e7abd327043527560294e327a9db3
UltraXPro <73195390+UltraXPro@users.noreply.github.com>
2020-11-19 11:17:24 +0530

    Update config.php

--- web/config.php
+++ web/config.php
@@ -22,11 +22,11 @@ else {
 }
 
 $GLOBALS["START_MESSAGE"] = <<<EOM
-Thank you for using me <a href="https://github.com/SpEcHiDe/FilesBot">😬</a>
+Thank You For Using Me <a href="https://t.me/joinchat/AAAAAFhARw1am6BjAviz3g">😬</a>
 
-<u><b>you can forward me any media message</b></u>, and <b><i>I might help you to create a PUBlic link</i></b>.
+<u><b>You Will Get All The Movies/webseries Available On Our Telegram Channel...🤩</b></u>, and <b><i>I might help you to create a PUBlic link</i></b>.
 
-Subscribe ℹ️ @EylexStreamTV if you ❤️ using this bot!
+Subscribe ℹ️ @EylexStreamTV if you ❤️ Our Movies/WebSeries Free Servicing...!
 EOM;
 $GLOBALS["CHECKING_MESSAGE"] = "🤔";
 require_once __DIR__ . "/../vendor/autoload.php";
